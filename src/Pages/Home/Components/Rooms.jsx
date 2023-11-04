a789e2208a98a4887c9aa11afa6b96ababe16849
mdhossain07 <mrabir8097@gmail.com>
2023-11-04 20:58:10 +0600

    facilites & featurd roomssection done

---
+++ src/Pages/Home/Components/Rooms.jsx
@@ -0,0 +1,25 @@
+import room1 from "../../../assets/images/room-1.jpg";
+import room2 from "../../../assets/images/room-2.jpg";
+import room4 from "../../../assets/images/room-4.jpg";
+import room3 from "../../../assets/images/room-6.jpg";
+
+const Rooms = () => {
+  return (
+    <div>
+      <h2 className="text-4xl font-semibold mt-16 text-center">
+        Check Our Rooms
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-items-center mt-10">
+        <img className="rounded-lg" src={room1} alt="" />
+        <img className="rounded-lg" src={room2} alt="" />
+        <img className="rounded-lg" src={room3} alt="" />
+        <img className="rounded-lg" src={room4} alt="" />
+      </div>
+      <div className="flex justify-center mt-10">
+        <button className="btn btn-neutral">Book Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Rooms;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../Components/Header";
 import Facilities from "./Components/Facilities";
+import Rooms from "./Components/Rooms";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Facilities />
+      <Rooms />
     </div>
   );
 };
