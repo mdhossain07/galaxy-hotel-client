55ffb61022e485859f9bf1df80b1be1d1270722a
mdhossain07 <mrabir8097@gmail.com>
2023-11-05 12:30:23 +0600

    google authentication done

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+
 const Rooms = () => {
+  const { data } = useQuery({
+    queryKey: ["roomsData"],
+    queryFn: () => {
+      fetch("http://localhost:5001/rooms").then((res) => res.json());
+    },
+  });
+
+  console.log(data);
   return (
     <div>
       <h2>Rooms</h2>
--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOnD5EgYl49QI_Xw05qjkk__vkdUhzEdc",
-  authDomain: "galaxy-hotel-e2efd.firebaseapp.com",
-  projectId: "galaxy-hotel-e2efd",
-  storageBucket: "galaxy-hotel-e2efd.appspot.com",
-  messagingSenderId: "271325571831",
-  appId: "1:271325571831:web:19e216ecc52906e91181f2",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
